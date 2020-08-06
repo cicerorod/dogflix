7bee144d2d162923d6e6eeba2358ea1c9f4a7387
cicerorod <cicerorod@gmail.com>
2020-08-06 02:00:12 -0300

    ajustes para publicação no heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
 const URL_BACKEND = window.location.hostname.includes("localhost")
   ? "http://localhost:8080"
-  : "https://garoupicks.herokuapp.com";
+  : "https://dogflixcic.herokuapp.com";
 
 export default { URL_BACKEND };
+
+// https://dogflixcic.herokuapp.com/categorias
+// https://dogflixcic.herokuapp.com/videos
+// https://dogflixcic.herokuapp.com/categorias?_embed=videos
