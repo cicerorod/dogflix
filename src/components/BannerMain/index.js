7c1ef855b0e1a250762136de67f54e6eefc362ce
cicerorod <cicerorod@gmail.com>
2020-08-06 04:47:30 -0300

    ajuste no link do video para outras telas

--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+import { Underline } from "styled-icons/fa-solid";
 import VideoIframeResponsive from "./components/VideoIframeResponsive";
 import {
   BannerMainContainer,
   ContentAreaContainer,
   WatchButton,
+  WatchButtonLink,
 } from "./styles";
 
 function getYouTubeId(youtubeURL) {
@@ -13,11 +17,10 @@ function getYouTubeId(youtubeURL) {
   );
 }
 
-// https://i.ytimg.com/vi/7uP2kp9qZaE/maxresdefault.jpg
-
 export default function BannerMain({ videoTitle, videoDescription, url }) {
   const youTubeID = getYouTubeId(url);
   const bgUrl = `https://img.youtube.com/vi/${youTubeID}/maxresdefault.jpg`;
+  const urlVideo = url;
   // const bgUrl = "https://img.youtube.com/vi/7uP2kp9qZaE/hqdefault.jpg";
 
   return (
@@ -33,9 +36,27 @@ export default function BannerMain({ videoTitle, videoDescription, url }) {
 
         <ContentAreaContainer.Item>
           <VideoIframeResponsive youtubeID={youTubeID} />
-          <WatchButton>Assistir</WatchButton>
+          {/* <WatchButton
+            href="https://github.com/styled-components/styled-components"
+            target="_blank"
+            rel="noopener"
+            primary
+          >
+            Assistir
+          </WatchButton> */}
+
+          <WatchButtonLink
+            href={urlVideo}
+            target="_blank"
+            rel="noopener"
+            primary
+          >
+            Assistir
+          </WatchButtonLink>
         </ContentAreaContainer.Item>
       </ContentAreaContainer>
     </BannerMainContainer>
   );
 }
+// href="https://www.youtube.com/watch?v=7uP2kp9qZaE"
+//            target="_blank"
